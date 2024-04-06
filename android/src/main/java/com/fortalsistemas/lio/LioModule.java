eb738285e026095bdb69bb934483d2065bc046fd
Rafael Bravo <rafael@metheora.com>
2024-04-06 16:41:28 -0300

    fix android color import

--- android/src/main/java/com/fortalsistemas/lio/LioModule.java
+++ android/src/main/java/com/fortalsistemas/lio/LioModule.java
@@ -11,6 +11,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import android.graphics.Color;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
